a1d8ef3395ca104443d0498976eb1b5459fead83
paramtamtam <slashkot@gmail.com>
2017-10-28 00:32:39 +0500

    Extension interface fix

--- src/ValidationExtensionInterface.php
+++ src/ValidationExtensionInterface.php
@@ -2,13 +2,32 @@
 
 namespace AvtoDev\ExtendedLaravelValidator;
 
-use Illuminate\Contracts\Validation\Rule as ValidationRule;
-
 /**
  * Interface ValidationExtensionInterface.
  */
-interface ValidationExtensionInterface extends ValidationRule
+interface ValidationExtensionInterface
 {
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     *
+     * @see \Illuminate\Contracts\Validation\Rule
+     *
+     * @return bool
+     */
+    public function passes($attribute, $value);
+
+    /**
+     * Get the validation error message.
+     *
+     * @see \Illuminate\Contracts\Validation\Rule
+     *
+     * @return string
+     */
+    public function message();
+
     /**
      * Возвращает имя правила валидатора.
      *

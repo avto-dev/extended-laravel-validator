dd742a4540974e649ddf0bef7a32513bb1db1cda
Paramtamtam <slashkot@gmail.com>
2017-10-27 19:32:49 +0000

    Apply fixes from StyleCI

--- src/ValidationExtensionInterface.php
+++ src/ValidationExtensionInterface.php
@@ -10,8 +10,8 @@ interface ValidationExtensionInterface
     /**
      * Determine if the validation rule passes.
      *
-     * @param  string  $attribute
-     * @param  mixed  $value
+     * @param string $attribute
+     * @param mixed  $value
      *
      * @see \Illuminate\Contracts\Validation\Rule
      *

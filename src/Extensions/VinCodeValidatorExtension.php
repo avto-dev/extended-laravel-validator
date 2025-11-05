634bcd9b78f4c37d62db1fd3df659a40b349f018
yurichechulin <chechulin.u@spectrumdata.ru>
2025-11-05 11:22:48 +0500

    Change validation rules

--- src/Extensions/VinCodeValidatorExtension.php
+++ src/Extensions/VinCodeValidatorExtension.php
@@ -37,20 +37,17 @@ class VinCodeValidatorExtension extends AbstractValidatorExtension
 
         // Если значение в стеке уже есть - то просто возвращаем его
         if (! isset($stack[$value])) {
-            // Значение в верхнем регистре
-            $uppercase = Str::upper($value);
-
             // Удаляем все символы, кроме разрешенных
-            $cleared = \preg_replace('~[^0-9ABCDEFGHJKLMNPRSTUVWXYZ]~', '', $uppercase);
+            $cleared = \preg_replace('~[^0-9ABCDEFGHJKLMNPRSTUVWXYZ]~', '', $value);
 
             $stack[$value] = (
                 Str::length($value) === 17 // Длинна соответствует
-                && $uppercase === $cleared // После удаления запрещенных символов - значение не изменилось
-                && \preg_match('~[A-Z]~', $uppercase) === 1 // Содержит символы
+                && $value === $cleared // После удаления запрещенных символов - значение не изменилось
+                && \preg_match('~[A-Z]~', $value) === 1 // Содержит символы
                 && \preg_match('~\d~', $value) === 1 // Содержит числа
-                && ! Str::contains($uppercase, ['I', 'O', 'Q']) // Не содержит запрещенные символы
-                && \is_numeric(Str::substr($uppercase, -4, 4)) // Последние четыре символа обязательно числа
-                && preg_match('/^(?=.*[a-zA-Z])(?=.*[1-9]).*$/', $uppercase)  // Строка содержит хотя бы одну букву и одну цифру, отличную от нуля
+                && ! Str::contains($value, ['I', 'O', 'Q']) // Не содержит запрещенные символы
+                && \is_numeric(Str::substr($value, -4, 4)) // Последние четыре символа обязательно числа
+                && preg_match('/^(?=.*[A-Z])(?=.*[1-9]).*$/', $value)  // Строка содержит хотя бы одну букву и одну цифру, отличную от нуля
             );
         }
 
--- tests/Extensions/VinCodeValidatorExtensionTest.php
+++ tests/Extensions/VinCodeValidatorExtensionTest.php
@@ -72,6 +72,12 @@ class VinCodeValidatorExtensionTest extends AbstractExtensionTestCase
             'JTEHТ05J602091383',
             'XTА21124070486756',
 
+            // Содержащие строчные буквы
+            'Xw8ZZZ61ZLG028617',
+            '5uxfa13585ly08847',
+            'wbafw51040C245397',
+            'z94cb41abdr105897',
+
             // Содержащие запрещенные символы
             'XTAIS015LG0916164',
             'JTHBK2623620O3591',
@@ -98,7 +104,7 @@ class VinCodeValidatorExtensionTest extends AbstractExtensionTestCase
 
             // Должна быть хотя бы одна цифра отличная от нуля и хотя бы одна буква
             '00000000000000001',
-            'AAAAAAAAAAAAAAAA',
+            'AAAAAAAAAAAAAAAAA',
             'A0000000000000000',
 
             // Последние четыре символа должны быть цифрами.
@@ -115,11 +121,6 @@ class VinCodeValidatorExtensionTest extends AbstractExtensionTestCase
     protected function getValidValues(): array
     {
         return [
-            // В нижнем регистре
-            '5uxfa13585ly08847',
-            'wbafw51040C245397',
-            'z94cb41abdr105897',
-
             '5UXFA13585LY08847',
             'WBAFW51040C245397',
             'Z94CB41ABDR105897',
@@ -222,7 +223,6 @@ class VinCodeValidatorExtensionTest extends AbstractExtensionTestCase
             'XWF0AHM75B0002747',
             'XW8ZZZ61ZLG028617',
             'A0000000000000001',
-
         ];
     }
 }
